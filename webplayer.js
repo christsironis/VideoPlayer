ba5475c9ce4a1f1bfe016e812edba1f07c97dbde
chris <christsiroiss@gmail.com>
2022-03-26 01:58:40 +0200

    audio gain

--- webplayer.js
+++ webplayer.js
@@ -1,5 +1,7 @@
+let videoCont = document.querySelector("#videoPlayerCont");
 let video = document.querySelector("#videoPlayer");
 let playBut = document.querySelector("#playpause");
+let playButPath = playBut.querySelector("#play");
 let vol = document.querySelector("#volume");
 let volBut = document.querySelector("#volume #vol-but");
 let currentTime = document.querySelector("#currentTime");
@@ -8,4 +10,63 @@ let subs = document.querySelector("#subs");
 let settings = document.querySelector("#settings");
 let pip = document.querySelector("#pip");
 let fsBut = document.querySelector("#fullscreen");
+let fsButPaths = document.querySelectorAll("#fullscreen path");
 let progBarPin = document.querySelector("#progress-bar #bar-pin");
+
+function AudioGain(x){
+    let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    let source = audioCtx.createMediaElementSource(video);
+    let gainNode = audioCtx.createGain();
+    source.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+    gainNode.gain.value = x;
+}
+// AudioGain(15)
+
+playBut.addEventListener("click",PlayHandler);
+video.addEventListener('play', (event) => {
+    playBut.setAttribute("data-state","pause");
+});
+video.addEventListener('pause', (event) => {
+    playBut.setAttribute("data-state","play");
+});
+function PlayHandler(){
+    if(playBut.getAttribute("data-state") === "play"){
+        video.play();
+    } else{
+        video.pause();
+    }
+}
+
+fsBut.addEventListener("click",FullScrHandler);
+video.addEventListener("dblclick",FullScrHandler);
+addEventListener('fullscreenchange', event => {
+    if( document.fullscreenElement === videoPlayerCont){
+        fsBut.setAttribute("data-state","nofull");
+    } 
+    else if(document.fullscreenElement === null){
+        fsBut.setAttribute("data-state","full");
+    }
+});
+function FullScrHandler(){
+    if( document.fullscreenElement === videoPlayerCont){
+        document.exitFullscreen();
+    } 
+    else if(document.fullscreenElement === null){
+        videoCont.requestFullscreen();
+    }
+}
+
+pip.addEventListener("click",PipHandler);
+addEventListener('leavepictureinpicture', event => { 
+    pip.setAttribute("data-state","disabled");
+});
+function PipHandler(){
+    if(document.pictureInPictureElement === null && document.pictureInPictureEnabled){
+        pip.setAttribute("data-state","enabled");
+        video.requestPictureInPicture();
+    } else if(document.pictureInPictureElement === video){
+        pip.setAttribute("data-state","disabled");
+        document.exitPictureInPicture();
+    }
+}
