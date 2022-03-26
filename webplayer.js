da301b14561804af8fb60d598fa602bc7550e8e6
chris <christsiroiss@gmail.com>
2022-03-26 15:41:57 +0200

    audio con

--- webplayer.js
+++ webplayer.js
@@ -4,6 +4,7 @@ let playBut = document.querySelector("#playpause");
 let playButPath = playBut.querySelector("#play");
 let vol = document.querySelector("#volume");
 let volBut = document.querySelector("#volume #vol-but");
+let volPin = document.querySelector("#volume #vol-percent-bar #vol-pin");
 let currentTime = document.querySelector("#currentTime");
 let totalTime = document.querySelector("#totalTime");
 let subs = document.querySelector("#subs");
@@ -13,15 +14,27 @@ let fsBut = document.querySelector("#fullscreen");
 let fsButPaths = document.querySelectorAll("#fullscreen path");
 let progBarPin = document.querySelector("#progress-bar #bar-pin");
 
-function AudioGain(x){
+// function AudioGain( audio, x ){
     let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     let source = audioCtx.createMediaElementSource(video);
     let gainNode = audioCtx.createGain();
     source.connect(gainNode);
     gainNode.connect(audioCtx.destination);
-    gainNode.gain.value = x;
+    gainNode.gain.value = 1;
+// }
+// AudioGain( video, 15 )
+
+// video.volume=0.2;
+if(gainNode.gain.value > 1){
+
+    console.log((gainNode.gain.value /15)*50 +50)
+    volPin.style.setProperty("--volumePos",(gainNode.gain.value /15)*50+ 50+"px");
+}else{
+    console.log(gainNode.gain.value*50)
+    volPin.style.setProperty("--volumePos",(gainNode.gain.value )*50+"px");
+
 }
-// AudioGain(15)
+
 
 playBut.addEventListener("click",PlayHandler);
 video.addEventListener('play', (event) => {
